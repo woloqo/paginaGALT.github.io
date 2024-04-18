4a38603e9058a806f1a679efa5bf0313153aa41a
Alejandro López <97767067+woloqo@users.noreply.github.com>
2024-04-17 23:19:25 -0700

    Doom 0.2
    Primera implementacion del trazado de rayos, aun no hay renderizado... pronto, pronto.

--- js/raycasting.js
+++ js/raycasting.js
@@ -5,6 +5,7 @@ var FPS = 30;
 //Dimension del canvas en pixeles
 var cWidth  = 500;
 var cHeight = 500;
+var tamTile = 50;
 
 //Nivel
 var escenario;
@@ -29,38 +30,36 @@ var lvl1 = [
 
 //Input
 document.addEventListener('keydown', function(tecla){
-    switch(tecla.keyCode){
-        case 38:
+    switch(tecla.code){
+        case "KeyW":
             jugador.up();
             break;
-        case 40:
+        case "KeyS":
             jugador.down();
             break;
-        case 39:
+        case "ArrowRight":
             jugador.right();
             break;
-        case 37:
+        case "ArrowLeft":
             jugador.left();
             break;
     }
 });
 document.addEventListener('keyup', function(tecla){
-
-    switch(tecla.keyCode){
-        case 38:
+    switch(tecla.code){
+        case "KeyW":
             jugador.stopMoving();
             break;
-        case 40:
+        case "KeyS":
             jugador.stopMoving();
             break;
-        case 39:
+        case "ArrowRight":
             jugador.stopRotating();
             break;
-        case 37:
+        case "ArrowLeft":
             jugador.stopRotating();
             break;
     }
-
 });
 
 //Normalizar angulo
@@ -70,6 +69,165 @@ function normalize(angulo){
     return angulo;
 }
 
+function distanciaEntrePuntos(x1, y1, x2, y2){
+    return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+}
+
+//Clase rayo
+class Ray{
+    constructor(con, escenario, x, y, anguloJugador, incrementoAngulo, columna){
+        this.incrementoAngulo = incrementoAngulo;
+        this.angulo = anguloJugador;
+        this.escenario = escenario;
+        this.columna = columna;
+        this.wallHitXHor = 0;
+        this.wallHitYHor = 0;
+        this.wallHitXVer = 0;
+        this.wallHitYVer = 0;
+        this.wallHitX = 0;
+        this.wallHitY = 0;
+        this.ctx = con;
+        this.x = x;
+        this.y = y;
+    }
+
+    setAngulo(angulo){
+        this.angulo = normalize(angulo + this.incrementoAngulo);
+    }
+
+    cast(){
+        this.xIntercept = 0;
+        this.yIntercept = 0;
+        
+        this.abajo = false;
+        this.izq   = false;
+
+        this.xStep = 0;
+        this.yStep = 0;
+
+        if(this.angulo < Math.PI) this.abajo = true;
+        if(this.angulo > Math.PI / 2 && this.angulo < 3 * Math.PI / 2) this.izq = true;
+
+        //Colision orisontal (ya se q se escribe horizontal)
+        var choqueHor = false;
+
+        this.yIntercept = Math.floor(this.y / tamTile) * tamTile;
+
+        //Si apunta hacia abajo, aumenta uno
+        if(this.abajo) this.yIntercept += tamTile;
+
+        var ady = (this.yIntercept - this.y) / Math.tan(this.angulo);
+        this.xIntercept = this.x + ady;
+
+        this.yStep = tamTile;
+        this.xStep = this.yStep / Math.tan(this.angulo);
+
+        //si vamos hacia arriba invertimos ystep
+        if(!this.abajo) this.yStep = -this.yStep;
+
+        //Comprobar que el xstep es coherente
+        if((this.izq && this.xStep > 0) || (!this.izq && this.xStep < 0)) this.xStep = - this.xStep;
+
+        var sigXHor = this.xIntercept;
+        var sigYHor = this.yIntercept;
+
+        //si apunta hacia arriba, se resta un pixel para forzar la colision
+        if(!this.abajo) sigYHor--;
+
+        //Ahora si buscamos la fakin colision hroziotnal
+        while(!choqueHor){
+            //sacamos la casilla 
+            var casX = parseInt(sigXHor/tamTile);
+            var casY = parseInt(sigYHor/tamTile);
+
+            if(this.escenario.collision(casX, casY)){
+                choqueHor = true;
+                this.wallHitXHor = sigXHor;
+                this.wallHitYHor = sigYHor;
+            }
+            else{
+                sigXHor += this.xStep;
+                sigYHor += this.yStep;
+            }
+        }
+
+        //Colision vertical
+        var choqueVer = false;
+
+        //buscamos la primera inter
+        this.xIntercept = Math.floor(this.x / tamTile) * tamTile;
+
+        //Si apunta a la derecha se incrementa 1
+        if(!this.izq) this.xIntercept += tamTile;
+
+        //Se le suma el cateto opuesto
+        var opuesto = (this.xIntercept - this.x) * Math.tan(this.angulo);
+        this.yIntercept = this.y + opuesto;
+
+        //calcula la distancia de cada paso
+        this.xStep = tamTile;
+        if(this.izq) this.xStep = -this.xStep;
+
+        this.yStep =  tamTile * Math.tan(this.angulo);
+        if((!this.abajo && this.yStep > 0) || (this.abajo && this.yStep < 0)) this.yStep = -this.yStep;
+
+        var sigXVer = this.xIntercept;
+        var sigYVer = this.yIntercept;
+
+        if(this.izq) sigXVer--;
+
+        //buscamos la fakin colision vertiuacal;l
+        while(!choqueVer && (sigXVer >= 0 && sigYVer >= 0 && sigXVer < cWidth && sigYVer < cHeight)){
+            //obtenemos la casilla 
+            var casillaX = parseInt(sigXVer/tamTile);
+            var casillaY = parseInt(sigYVer/tamTile);
+
+            if(this.escenario.collision(casillaX, casillaY)){
+                choqueVer = true;
+                this.wallHitXVer = sigXVer;
+                this.wallHitYVer = sigYVer;
+            }
+            else{
+                sigXVer += this.xStep;
+                sigYVer += this.yStep;
+            }
+        }
+
+        var distanciaHorizontal = 999999999;
+        var distanciaVertical   = 999999999;
+
+        if(choqueHor){
+            distanciaHorizontal = distanciaEntrePuntos(this.x, this.y, this.wallHitXHor, this.wallHitYHor);
+        }
+        if(choqueVer){
+            distanciaVertical = distanciaEntrePuntos(this.x, this.y, this.wallHitXVer, this.wallHitYVer);
+        }
+
+        //checamos la colision mas cercana entre la colision x e y
+        if(distanciaHorizontal < distanciaVertical){
+            this.wallHitX = this.wallHitXHor;
+            this.wallHitY = this.wallHitYHor;
+        }
+        else{
+            this.wallHitX = this.wallHitXVer;
+            this.wallHitY = this.wallHitYVer;
+        }
+    }
+
+    draw(){
+        //Dibuja el rayo
+        this.cast();
+
+        var xDest = this.wallHitX;
+        var yDest = this.wallHitY;
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x, this.y);
+        this.ctx.lineTo(xDest, yDest);
+        this.ctx.strokeStyle = 'red';
+        this.ctx.stroke();
+    }
+}
 
 //Clase escenario
 class Level{
@@ -80,21 +238,16 @@ class Level{
         this.ctx = con;
         
         //Dimensiones matriz
-        this.heightM = this.matriz.length;
-        this.widthM  = this.matriz[0].length;
+        this.anchoM  = this.matriz[0].length;
+        this.altoM   = this.matriz.length;
 
         //Dimensiones del canvas
-        this.heightC = this.canvas.height;
-        this.widthC  = this.canvas.width;
+        this.altoC = this.canvas.height;
+        this.anchoC  = this.canvas.width;
 
         //Dimensiones de los tiles
-        this.heightT = parseInt(this.heightC / this.heightM);
-        this.widthT  = parseInt(this.widthC  / this.widthM);
-
-        // console.log(this.matriz);
-        // console.log(this.heightM);
-        // console.log(this.heightC);
-        // console.log(this.heightT);
+        this.anchoT  = parseInt(this.anchoC  / this.anchoM);
+        this.altoT   = parseInt(this.altoC   / this.altoM);
     }
 
     collision(x, y){
@@ -105,13 +258,13 @@ class Level{
 
     draw(){
         var color;
-        for(var i=0; i<this.heightM; i++){
-            for(var j=0; j<this.widthM;j++){
+        for(var i=0; i<this.altoM; i++){
+            for(var j=0; j<this.anchoM;j++){
                 if(this.matriz[i][j] == 1) color = wallColor;
                 else color = floorColor;
 
                 this.ctx.fillStyle = color;
-                this.ctx.fillRect(j*this.widthT, i*this.heightT, this.widthT, this.heightT);
+                this.ctx.fillRect(j*this.anchoT, i*this.altoT, this.anchoT, this.altoT);
             }
         }
     }
@@ -128,77 +281,64 @@ class Player{
         this.x = x;
         this.y = y;
 
-        this.rotate   = 0; // -1 = izq  ~ 1 = der
-        this.move     = 0; // 0 = parao ~ 1 = aelante ~ -1 atra
+        this.rotar   = 0; // -1 = izq  ~ 1 = der
+        this.mover     = 0; // 0 = parao ~ 1 = aelante ~ -1 atra
 
-        this.rotatingAngle = 0;
-        this.movementVel   = 3;                   //En pixeles;
-        this.rotateVel     = 3 * (Math.PI / 180); //Grados
+        this.anguloRotacion = 0;
+        this.velMov   = 3;                   //En pixeles;
+        this.velRot   = 3 * (Math.PI / 180); //Grados
+
+        this.rayo = new Ray(this.ctx, this.escenario, this.x, this.y, this.anguloRotacion, 0);
 
     }
 
-    up(){
-        //console.log("arriba");
-        this.move = 1;
-    }
-
-    down(){
-        //console.log("abajo");
-        this.move = -1;
-    }
-
-    left(){
-        //console.log("izq");
-        this.rotate = -1;
-    }
-
-    right(){
-        //console.log("der");
-        this.rotate = 1;
-    }
-
-    stopMoving(){
-        this.move = 0;
-    }
-
-    stopRotating(){
-        this.rotate = 0;
-    }
+    up(){ this.mover = 1; }
+    down(){ this.mover = -1; }
+    left(){ this.rotar = -1; }
+    right(){ this.rotar = 1; }
+    stopMoving(){ this.mover = 0; }
+    stopRotating(){ this.rotar = 0; }
 
     collision(x, y){
         var collide = false;
 
-        var casillaY = parseInt(y/this.escenario.heightT);
-        var casillaX = parseInt(x/this.escenario.widthT);
+        var casillaY = parseInt(y/this.escenario.altoT);
+        var casillaX = parseInt(x/this.escenario.anchoT);
 
         if(this.escenario.collision(casillaX,casillaY)) collide = true;
         return collide;
     }
 
     update(){
-        var newX = this.x + this.move * Math.cos(this.rotatingAngle) * this.movementVel;
-        var newY = this.y + this.move * Math.sin(this.rotatingAngle) * this.movementVel;
+        //movimiento
+        var newX = this.x + this.mover * Math.cos(this.anguloRotacion) * this.velMov;
+        var newY = this.y + this.mover * Math.sin(this.anguloRotacion) * this.velMov;
 
         if(!this.collision(newX, newY)){
             this.x = newX;
             this.y = newY;
         }
 
-        this.rotatingAngle += this.rotate * this.rotateVel;
-        this.rotatingAngle = normalize(this.rotatingAngle);
-        //console.log(this.rotatingAngle);
+        //giro
+        this.anguloRotacion += this.rotar * this.velRot;
+        this.anguloRotacion = normalize(this.anguloRotacion);
     }
-
+    
     draw(){
         this.update();
+        //actualiza el angulo
+        this.rayo.setAngulo(this.anguloRotacion);
+        this.rayo.x = this.x;
+        this.rayo.y = this.y;
+        this.rayo.draw();
 
         //Cuadro
         this.ctx.fillStyle = playerColor;
         this.ctx.fillRect(this.x-3, this.y-3, 6, 6);
 
         //Angulo
-        var xDes = this.x + Math.cos(this.rotatingAngle) * 20;
-        var yDes = this.y + Math.sin(this.rotatingAngle) * 20;
+        var xDes = this.x + Math.cos(this.anguloRotacion) * 20;
+        var yDes = this.y + Math.sin(this.anguloRotacion) * 20;
 
         this.ctx.beginPath();
         this.ctx.moveTo(this.x, this.y);
